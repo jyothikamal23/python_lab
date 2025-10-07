f3891811ce229f410d3264432716707f1d014e5c
Kamalakar <44663613+jyothikamal23@users.noreply.github.com>
2025-10-07 23:13:47 +0530

    Add files via upload

---
+++ PerformBasicMathematicalOperations.py
@@ -0,0 +1,9 @@
+#Takes two numbers as input from the user.
+a=int(input("Enter the first number:"))
+b=int(input("Enter thr secound number:"))
+
+# Performs the basic mathematical operations on these two numbers
+print("Adition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
---
+++ PersonalizedGreeting.py
@@ -0,0 +1,6 @@
+# 1.  Takes a user's first name and last name as input.
+first_name=str(input("Enter first name:"))
+last_name=str(input("Enter last name:"))
+print("Hello, "+first_name+" " +last_name+"! Welcome to the python program.")
+# 2.  Concatenates the first name and last name into a full name.
+# 3.  Prints a personalized greeting message using the full name.
